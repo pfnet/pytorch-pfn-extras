1fd2fbad8541eeab21c1c1ecdf6e93fdbce8e08a
niboshi <niboshi000@gmail.com>
2020-01-24 08:17:27 +0000

    Rewrite test

--- tests/pytorch_extensions_tests/training_tests/test_extension.py
+++ tests/pytorch_extensions_tests/training_tests/test_extension.py
@@ -1,72 +1,74 @@
-import unittest
-
 import pytest
+import torch
 
-from chainer import testing
-from chainer import training
+import pytorch_extensions as pe
 
 
-class TestExtension(unittest.TestCase):
+def _get_dummy_manager():
+    model = torch.nn.Module()
+    return pe.training.ExtensionsManager(
+        {'main': model},
+        [],  # optimizers
+        10,  # max_epochs
+        [],  # extensions
+    )
 
-    def test_raise_error_if_call_not_implemented(self):
-        class MyExtension(training.Extension):
+
+def test_raise_error_if_call_not_implemented():
+    class MyExtension(pe.training.Extension):
+        pass
+
+    ext = MyExtension()
+    trainer = _get_dummy_manager()
+    with pytest.raises(NotImplementedError):
+        ext(trainer)
+
+
+def test_default_name():
+    class MyExtension(pe.training.Extension):
+        pass
+
+    ext = MyExtension()
+    assert ext.default_name == 'MyExtension'
+
+
+def test_deleted_invoke_before_training():
+    class MyExtension(pe.training.Extension):
+        pass
+
+    ext = MyExtension()
+    with pytest.raises(AttributeError):
+        ext.invoke_before_training
+
+
+def test_make_extension():
+    def initialize(trainer):
+        pass
+
+    @pe.training.make_extension(trigger=(2, 'epoch'), default_name='my_ext',
+                                priority=50, initializer=initialize)
+    def my_extension(trainer):
+        pass
+
+    assert my_extension.trigger == (2, 'epoch')
+    assert my_extension.default_name == 'my_ext'
+    assert my_extension.priority == 50
+    assert my_extension.initialize is initialize
+
+
+def test_make_extension_default_values():
+    @pe.training.make_extension()
+    def my_extension(trainer):
+        pass
+
+    assert my_extension.trigger == (1, 'iteration')
+    assert my_extension.default_name == 'my_extension'
+    assert my_extension.priority == pe.training.PRIORITY_READER
+    assert my_extension.initialize is None
+
+
+def test_make_extension_unexpected_kwargs():
+    with pytest.raises(TypeError):
+        @pe.training.make_extension(foo=1)
+        def my_extension(_):
             pass
-
-        ext = MyExtension()
-        trainer = testing.get_trainer_with_mock_updater()
-        with pytest.raises(NotImplementedError):
-            ext(trainer)
-
-    def test_default_name(self):
-        class MyExtension(training.Extension):
-            pass
-
-        ext = MyExtension()
-        self.assertEqual(ext.default_name, 'MyExtension')
-
-    def test_deleted_invoke_before_training(self):
-        class MyExtension(training.Extension):
-            pass
-
-        ext = MyExtension()
-        with self.assertRaises(AttributeError):
-            ext.invoke_before_training
-
-    def test_make_extension(self):
-        def initialize(trainer):
-            pass
-
-        @training.make_extension(trigger=(2, 'epoch'), default_name='my_ext',
-                                 priority=50, initializer=initialize)
-        def my_extension(trainer):
-            pass
-
-        self.assertEqual(my_extension.trigger, (2, 'epoch'))
-        self.assertEqual(my_extension.default_name, 'my_ext')
-        self.assertEqual(my_extension.priority, 50)
-        self.assertIs(my_extension.initialize, initialize)
-
-    def test_make_extension_default_values(self):
-        @training.make_extension()
-        def my_extension(trainer):
-            pass
-
-        self.assertEqual(my_extension.trigger, (1, 'iteration'))
-        self.assertEqual(my_extension.default_name, 'my_extension')
-        self.assertEqual(my_extension.priority, training.PRIORITY_READER)
-        self.assertIsNone(my_extension.initialize)
-
-    def test_make_extension_deleted_argument(self):
-        with self.assertRaises(ValueError):
-            @training.make_extension(invoke_before_training=False)
-            def my_extension(_):
-                pass
-
-    def test_make_extension_unexpected_kwargs(self):
-        with self.assertRaises(TypeError):
-            @training.make_extension(foo=1)
-            def my_extension(_):
-                pass
-
-
-testing.run_module(__name__, __file__)
