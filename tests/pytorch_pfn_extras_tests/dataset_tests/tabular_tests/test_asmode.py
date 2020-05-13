88a60921b5cc4b784162e2b71c3349cc748e2f29
Emilio Castillo <ecastill@preferred.jp>
2020-05-13 02:34:44 +0000

    Added `TabularDataset`

---
+++ tests/pytorch_pfn_extras_tests/dataset_tests/tabular_tests/test_asmode.py
@@ -0,0 +1,36 @@
+import pytest
+
+import pytorch_pfn_extras as ppe
+from tests.pytorch_pfn_extras_tests.dataset_tests.tabular_tests import dummy_dataset  # NOQA
+
+
+@pytest.mark.parametrize(
+    'mode',
+    [tuple, dict, None]
+)
+def test_astuple(mode):
+    dataset = dummy_dataset.DummyDataset(mode=mode, convert=True)
+    view = dataset.astuple()
+    assert isinstance(view, ppe.dataset.TabularDataset)
+    assert len(view) == len(dataset)
+    assert view.keys == dataset.keys
+    assert view.mode == tuple
+    assert (
+        view.get_examples(None, None) == dataset.get_examples(None, None))
+    assert view.convert(view.fetch()) == 'converted'
+
+
+@pytest.mark.parametrize(
+    'mode',
+    [tuple, dict, None]
+)
+def test_asdict(mode):
+    dataset = dummy_dataset.DummyDataset(mode=mode, convert=True)
+    view = dataset.asdict()
+    assert isinstance(view, ppe.dataset.TabularDataset)
+    assert len(view) == len(dataset)
+    assert view.keys == dataset.keys
+    assert view.mode == dict
+    assert (
+        view.get_examples(None, None) == dataset.get_examples(None, None))
+    assert view.convert(view.fetch()) == 'converted'
