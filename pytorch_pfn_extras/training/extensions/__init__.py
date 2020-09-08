e3e072979fc844ceb64283303099f4331dd3d01f
corochann <acc1ssnn9terias@gmail.com>
2020-09-08 12:02:26 +0900

    Add ProgressBarNotebook

--- pytorch_pfn_extras/training/extensions/__init__.py
+++ pytorch_pfn_extras/training/extensions/__init__.py
@@ -7,6 +7,7 @@ from pytorch_pfn_extras.training.extensions.log_report import LogReport  # NOQA
 from pytorch_pfn_extras.training.extensions.micro_average import MicroAverage  # NOQA
 from pytorch_pfn_extras.training.extensions.print_report import PrintReport  # NOQA
 from pytorch_pfn_extras.training.extensions.progress_bar import ProgressBar  # NOQA
+from pytorch_pfn_extras.training.extensions.progress_bar_notebook import ProgressBarNotebook  # NOQA
 from pytorch_pfn_extras.training.extensions.parameter_statistics import ParameterStatistics  # NOQA
 from pytorch_pfn_extras.training.extensions.plot_report import PlotReport  # NOQA
 from pytorch_pfn_extras.training.extensions.value_observation import observe_lr  # NOQA
