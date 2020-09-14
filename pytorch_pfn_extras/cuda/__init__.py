174bf630ed89dcd9aac2586afc5976ecc93560ca
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-09-14 14:05:32 +0900

    flake8

--- pytorch_pfn_extras/cuda/__init__.py
+++ pytorch_pfn_extras/cuda/__init__.py
@@ -1,3 +1,3 @@
-from pytorch_pfn_extras.cuda._allocator import stream
-from pytorch_pfn_extras.cuda._allocator import use_torch_mempool_in_cupy
-from pytorch_pfn_extras.cuda._allocator import use_default_mempool_in_cupy
+from pytorch_pfn_extras.cuda._allocator import stream  # NOQA
+from pytorch_pfn_extras.cuda._allocator import use_torch_mempool_in_cupy  # NOQA
+from pytorch_pfn_extras.cuda._allocator import use_default_mempool_in_cupy  # NOQA
