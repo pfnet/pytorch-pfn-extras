4f60b67e46fd5cc9dfadeaa0b3551fbb48f538c6
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-11-30 18:35:49 +0900

    install torch

--- .flexci/test.bat
+++ .flexci/test.bat
@@ -1,3 +1,5 @@
+python -m pip install torch===1.7.0 torchvision===0.8.1 torchaudio===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+
 python -m pip install pytorch-ignite pytest flake8 matplotlib tensorboard onnx ipython ipywidgets pandas optuna
 python -m pip install cupy-cuda110
 python -m pip install -e .
