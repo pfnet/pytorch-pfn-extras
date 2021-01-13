08bb031c99fcd89e935304bfc21e3b32523a903e
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-01-13 02:49:05 +0000

    fix

--- .flexci/windows/test.ps1
+++ .flexci/windows/test.ps1
@@ -16,7 +16,7 @@ if ($test -eq "torch15") {
 } elseif ($test -eq "torch16") {
     # PyTorch 1.6 + Python 3.6
     ActivatePython 3.6
-    RunOrDie python -m pip install torch==1.6.* torchvision===0.7.* -f https://download.pytorch.org/whl/torch_stable.html
+    RunOrDie python -m pip install torch==1.6.* torchvision==0.7.* -f https://download.pytorch.org/whl/torch_stable.html
 
 } elseif ($test -eq "torch17") {
     # PyTorch 1.7 + Python 3.8
