ffcb8c6b6db5f359306e06bc7567ed5a33530138
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-03-08 09:56:14 +0000

    use curl command instead of internal alias

--- .flexci/windows/download_mnist.ps1
+++ .flexci/windows/download_mnist.ps1
@@ -5,8 +5,8 @@ $ErrorActionPreference = "Stop"
 
 New-Item ../data/MNIST/raw -ItemType Directory
 Push-Location ../data/MNIST/raw
-curl -LO http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-curl -LO http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-curl -LO http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-curl -LO http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+curl.exe -LO http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+curl.exe -LO http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+curl.exe -LO http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+curl.exe -LO http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
 Pop-Location
