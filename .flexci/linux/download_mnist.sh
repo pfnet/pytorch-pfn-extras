1844d7fdcd286b20c979f69fedcdcc725aa3407c
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-03-15 09:18:55 +0000

    adjust path

--- .flexci/linux/download_mnist.sh
+++ .flexci/linux/download_mnist.sh
@@ -8,4 +8,4 @@ set -uex
 #curl -LO http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
 #curl -LO http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
 mkdir -p mnist_raw
-gsutil -m cp -r "gs://chainer-artifacts-pfn-public-ci/pytorch-pfn-extras/mnist" mnist_raw
+gsutil -m cp -r "gs://chainer-artifacts-pfn-public-ci/pytorch-pfn-extras/mnist/*" mnist_raw
