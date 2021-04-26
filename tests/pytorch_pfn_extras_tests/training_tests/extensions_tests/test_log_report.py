5b5fdabbcc170f5aa659a5297996c5d36cedac53
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-04-26 11:39:57 +0000

    add test for LogReport

---
+++ tests/pytorch_pfn_extras_tests/training_tests/extensions_tests/test_log_report.py
@@ -0,0 +1,64 @@
+import json
+import os.path
+import tempfile
+
+import pytest
+import yaml
+
+import pytorch_pfn_extras as ppe
+from pytorch_pfn_extras.training import extensions
+
+@pytest.mark.parametrize(
+    'filename,expected_format',
+    [
+        ('out.json', 'json'),
+        ('out.xyz', 'json'),
+        (None, 'json'),
+        ('out.yaml', 'yaml'),
+        ('out.jsonl', 'json-lines'),
+    ]
+)
+def test_format_from_ext(filename, expected_format):
+    log_report = extensions.LogReport(filename=filename, format=None)
+    assert log_report._format == expected_format
+
+
+@pytest.mark.parametrize(
+    'format,append',
+    [
+        ('json', False),
+        ('json-lines', True),
+        ('json-lines', False),
+        ('yaml', True),
+        ('yaml', False),
+    ]
+)
+def test_output(format, append):
+    max_epochs = 3
+    iters_per_epoch = 5
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        manager = ppe.training.ExtensionsManager(
+            {}, {}, max_epochs=max_epochs, iters_per_epoch=iters_per_epoch,
+            out_dir=tmpdir)
+        log_report = extensions.LogReport(
+            filename='out', format=format, append=append)
+        manager.extend(log_report)
+        for epoch_idx in range(max_epochs):
+            for batch_idx in range(iters_per_epoch):
+                with manager.run_iteration():
+                    pass
+            with open(os.path.join(tmpdir, 'out')) as f:
+                data = f.read()
+                if format == 'json':
+                    values = json.loads(data)
+                elif format == 'json-lines':
+                    values = [json.loads(x) for x in data.splitlines()]
+                elif format == 'yaml':
+                    values = yaml.load(data, Loader=yaml.SafeLoader)
+                assert len(values) == epoch_idx + 1
+                this_epoch = values.pop()
+                assert this_epoch['epoch'] == epoch_idx + 1
+                assert (this_epoch['iteration']
+                        == (epoch_idx + 1) * iters_per_epoch)
+                assert 0 < this_epoch['elapsed_time']
