46e47d7c6b876077817fbd05c592684255212b34
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-04-26 15:26:35 +0000

    use markdown and pydata_sphinx_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ author = 'Preferred Networks, Inc.'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +45,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
