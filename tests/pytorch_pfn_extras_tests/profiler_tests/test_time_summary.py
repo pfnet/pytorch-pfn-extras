14504b7855d6cb69e5834b7eb9320186025959a2
Emilio Castillo <ecastill@preferred.jp>
2021-05-20 05:14:10 +0000

    add test

---
+++ tests/pytorch_pfn_extras_tests/profiler_tests/test_time_summary.py
@@ -0,0 +1,53 @@
+import multiprocessing as mp
+from pytorch_pfn_extras.profiler.time_summary import TimeSummary, time_summary
+
+
+def test_report():
+    summary = TimeSummary()
+    with summary.report("foo"):
+        pass
+    summary.wait()
+    with summary.summary() as s:
+        assert "foo" in s.compute_mean()
+    summary.close()
+
+
+def worker(summary):
+    with summary.report("foo"):
+        pass
+
+
+def test_report_from_other_process():
+    summary = TimeSummary()
+    p = mp.Process(target=worker, args=(summary,))
+    p.start()
+    p.join()
+    summary.wait()
+    with summary.summary() as s:
+        assert "foo" in s.compute_mean()
+    summary.close()
+
+
+def worker1():
+    with time_summary.report("foo"):
+        pass
+
+
+def test_global_summary():
+    p = mp.Process(target=worker1)
+    p.start()
+    p.join()
+    time_summary.wait()
+    with time_summary.summary() as s:
+        assert "foo" in s.compute_mean()
+
+
+def test_clear():
+    summary = TimeSummary()
+    summary._summary.add({"foo": 10})
+    summary.wait()
+    with summary.summary(clear=True) as s:
+        assert s.compute_mean() == {"foo": 10}
+    with summary.summary(clear=True) as s:
+        assert s.compute_mean() == {}
+    summary.close()
