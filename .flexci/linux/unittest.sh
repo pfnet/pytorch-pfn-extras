e93a0c6f7d776def777169a89bb6250c63490313
Emilio Castillo <ecastill@preferred.jp>
2021-07-07 07:31:56 +0000

    Run `test_distributed` independently

--- .flexci/linux/unittest.sh
+++ .flexci/linux/unittest.sh
@@ -5,11 +5,16 @@ set -uex
 # Install
 pip install .
 
+# Show shared memory
+df -h | grep shm
+
 # Show packages
 pip list
 
 # Run unit tests
-python -m pytest tests/
+python -m pytest tests/ -k 'not TestDistributedDataParallel'
+# Do it in two steps to avoid flakiness
+python -m pytest tests/tests/pytorch_pfn_extras_tests/nn_tests/parallel_tests/test_distributed.py
 
 # Run examples
 if [ -d mnist_raw ]; then
