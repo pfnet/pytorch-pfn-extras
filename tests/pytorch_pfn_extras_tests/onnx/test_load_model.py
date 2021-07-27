9e2c3b95d3ecc83523213de35ec88c120c9b94c1
Takeshi Watanabe <twata@preferred.jp>
2021-07-27 16:23:34 +0900

    Add tests to ppe.onnx.load_model

---
+++ tests/pytorch_pfn_extras_tests/onnx/test_load_model.py
@@ -0,0 +1,26 @@
+import os
+
+import pytest
+import torch
+
+import pytorch_pfn_extras.onnx as tou
+from tests.pytorch_pfn_extras_tests.onnx.test_export_testcase import Net
+
+
+@pytest.mark.filterwarnings("ignore:Named tensors .* experimental:UserWarning")
+def test_onnx_load_model():
+    model = Net()
+    outdir = "out/load_model_test"
+    tou.export_testcase(model, torch.rand(1, 1, 28, 28), outdir,
+                        training=True, do_constant_folding=False)
+    tou.load_model(os.path.join(outdir, "model.onnx"))
+
+
+@pytest.mark.filterwarnings("ignore:.*ONNX is stripped .*:UserWarning")
+def test_stripped_onnx_load_model():
+    model = Net()
+    outdir = "out/stripped_load_model_test"
+    tou.export_testcase(model, torch.rand(1, 1, 28, 28), outdir,
+                        strip_large_tensor_data=True, training=True,
+                        do_constant_folding=False)
+    tou.load_model(os.path.join(outdir, "model.onnx"))
