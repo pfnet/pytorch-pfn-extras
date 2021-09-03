5db13bdc311ee702c2af9785f9f9a330ecf6cc05
Akifumi Imanishi <imanishi@preferred.jp>
2021-09-03 16:48:26 +0000

    Fix for flake8

--- pytorch_pfn_extras/runtime/_registry.py
+++ pytorch_pfn_extras/runtime/_registry.py
@@ -1,4 +1,4 @@
-from typing import Dict, Type, TYPE_CHECKING
+from typing import Dict, Type
 
 import torch
 
--- pytorch_pfn_extras/runtime/_to.py
+++ pytorch_pfn_extras/runtime/_to.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional, Type, TypeVar, TYPE_CHECKING
+from typing import Any, Dict, Optional, Type, TypeVar
 
 import torch
 
