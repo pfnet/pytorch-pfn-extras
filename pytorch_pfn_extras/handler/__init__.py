cb552eb4eaf6c6f221f3948f043120b064f7bb11
emcastillo <ecastill@preferred.jp>
2021-09-13 18:16:58 +0900

    Update pytorch_pfn_extras/handler/__init__.py
    Co-authored-by: Kenichi Maehashi <webmaster@kenichimaehashi.com>

--- pytorch_pfn_extras/handler/__init__.py
+++ pytorch_pfn_extras/handler/__init__.py
@@ -1,2 +1,4 @@
 from pytorch_pfn_extras.handler._handler import BaseHandler, Handler  # NOQA
-from pytorch_pfn_extras.handler._logic import BaseLogic, Logic  # NOQA
+
+# Deprecated, only imported for backward compatibility
+from pytorch_pfn_extras.handler._logic import torch_autocast
