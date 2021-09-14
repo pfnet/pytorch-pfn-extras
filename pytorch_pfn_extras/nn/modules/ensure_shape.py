ece118ea9de3da5f829246a63a7f37f36ada20c0
Emilio Castillo <ecastill@preferred.jp>
2021-09-14 01:36:47 +0000

    Jit/TS tests

--- pytorch_pfn_extras/nn/modules/ensure_shape.py
+++ pytorch_pfn_extras/nn/modules/ensure_shape.py
@@ -33,13 +33,40 @@ class EnsureShapeAndDtype(torch.nn.Module):
         self._dtype = dtype
         self._broadcastable = broadcastable
         self._can_cast = can_cast
+        # Check if there are Nones in the shape and replace them by 1s
+        # so we can compare the shapes using broadcast semantics
+        if self._shape is not None and None in self._shape:
+            self._shape = tuple(x if x is not None else 1 for x in self._shape)
+            self._broadcastable = True
+
+        if self._shape is not None:
+            # This is required for torch script
+            self._shape = torch.tensor(self._shape)
+
+    def _broadcast(self, shape_1: torch.Tensor, shape_2: torch.Tensor):
+        # Torch broadcast_shapes raises an exception, we want a simple
+        # method that returs True/False so we can use torch script
+        l_1, l_2 = len(shape_1), len(shape_2)
+        if l_1 != l_2:
+            # we would like to do checks like the below one
+            # but torchscript converts everything to vectors
+            # shape_2 = (1,) * (l_1 - l_2) + shape_2
+            if l_1 < l_2:
+                shape_1 = torch.cat([torch.ones(l_2 - l_1), shape_1])
+            else:
+                shape_2 = torch.cat([torch.ones(l_1 - l_2), shape_2])
+        # Look for Elements that are different in the tensors
+        for a, b in zip(shape_1, shape_2):
+            if a != b and a != 1 and b != 1:
+                return False
+        return True
 
     def forward(self, input: torch.Tensor):
-        if self._shape is not None and input.shape != self._shape:
+        # To make it compatible with torchscript since torch.Size does not work
+        t_shape = torch.tensor(input.shape)
+        if self._shape is not None and list(t_shape) != list(self._shape):
             if self._broadcastable:
-                try:
-                    torch.broadcast_shapes(self._shape, input.shape)
-                except RuntimeError:
+                if not self._broadcast(t_shape, self._shape):
                     raise ValueError(
                         f'Shapes {self._shape} and {input.shape} are non'
                         ' broadcastable')
--- tests/pytorch_pfn_extras_tests/nn_tests/modules_tests/test_ensure_shape.py
+++ tests/pytorch_pfn_extras_tests/nn_tests/modules_tests/test_ensure_shape.py
@@ -5,7 +5,6 @@ from pytorch_pfn_extras.nn import EnsureShapeAndDtype
 
 
 class TestEnsureShapeAndDtype:
-
     def test_wrong_initialization(self):
         with pytest.raises(ValueError, match='both arguments'):
             EnsureShapeAndDtype(None, None)
@@ -30,9 +29,10 @@ class TestEnsureShapeAndDtype:
     @pytest.mark.parametrize('shape_t, shape_c', [
          ((1,), (2,)),
          ((1, 1), (2, 1)),
+         ((1, 1), (2,)),
          ((2, 1), (2, 2)),
-         ((2, 4), (1, 4)),
-         ((2, 3, 4), (2, 1, 4)),
+         ((2, 4), (4,)),
+         ((2, 3, 4), (1, 4)),
     ])
     def test_broadcastable_shape(self, shape_t, shape_c):
         tensor = torch.zeros(shape_t)
@@ -43,11 +43,35 @@ class TestEnsureShapeAndDtype:
          ((3,), (2,)),
          ((2, 3), (2, 2)),
          ((2, 4), (3, 4)),
+         ((2, 4), (3,)),
          ((2, 3, 4), (2, 2, 1)),
     ])
     def test_nonbroadcastable_shape(self, shape_t, shape_c):
         tensor = torch.zeros(shape_t)
         module = EnsureShapeAndDtype(shape_c, broadcastable=True)
+        with pytest.raises(ValueError, match='non broadcastable'):
+            module(tensor)
+
+    @pytest.mark.parametrize('shape_t, shape_c', [
+         ((2,), (None,)),
+         ((2, 2), (2, None)),
+         ((2, 1), (None, 2)),
+         ((1, 4), (None, 4)),
+         ((2, 3, 4), (2, None, 4)),
+    ])
+    def test_unknown_shape(self, shape_t, shape_c):
+        tensor = torch.zeros(shape_t)
+        module = EnsureShapeAndDtype(shape_c)
+        module(tensor)
+
+    @pytest.mark.parametrize('shape_t, shape_c', [
+         ((3, 2), (2, None)),
+         ((1, 4), (None, 2)),
+         ((2, 3, 4), (3, None, 4)),
+    ])
+    def test_invalid_unknown_shape(self, shape_t, shape_c):
+        tensor = torch.zeros(shape_t)
+        module = EnsureShapeAndDtype(shape_c)
         with pytest.raises(ValueError, match='non broadcastable'):
             module(tensor)
 
@@ -97,3 +121,36 @@ class TestEnsureShapeAndDtype:
         tensor = torch.zeros(shape, dtype=dtype)
         module = EnsureShapeAndDtype(shape, dtype)
         module(tensor)
+
+    # Too many warnings to list them all
+    @pytest.mark.filterwarnings('ignore')
+    def test_jit_module(self):
+        shape = (10, 5)
+        dtype = torch.float32
+        tensor = torch.zeros(shape, dtype=dtype)
+        module = EnsureShapeAndDtype(shape, dtype)
+        jit_module = torch.jit.trace(module, (tensor,))
+        jit_module(tensor)
+
+        # An invalid tensor also passes, because checks are disabled
+        shape = (5, 5)
+        tensor = torch.zeros(shape, dtype=dtype)
+        jit_module(tensor)
+
+        # Tracing with a different shape fails during trace process
+        with pytest.raises(ValueError, match='input shape is'):
+            jit_module = torch.jit.trace(module, (tensor,))
+
+    def test_torchscript_module(self):
+        shape = (10, 5)
+        dtype = torch.float32
+        tensor = torch.zeros(shape, dtype=dtype)
+        module = EnsureShapeAndDtype(shape, dtype)
+        jit_module = torch.jit.script(module)
+        jit_module(tensor)
+
+        shape = (5, 5)
+        tensor = torch.zeros(shape, dtype=dtype)
+        # torchscript changes the exception type
+        with pytest.raises(torch.jit.Error, match='input shape is'):
+            jit_module(tensor)
