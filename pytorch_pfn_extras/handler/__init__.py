246afe19fe7acd0bb3fc1f84f5352fdfb4c41864
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-09-14 10:55:00 +0900

    Fix import

--- pytorch_pfn_extras/handler/__init__.py
+++ pytorch_pfn_extras/handler/__init__.py
@@ -1,4 +1,5 @@
 from pytorch_pfn_extras.handler._handler import BaseHandler, Handler  # NOQA
+from pytorch_pfn_extras.handler._logic import BaseLogic, Logic  # NOQA
 
 # Deprecated, only imported for backward compatibility
 from pytorch_pfn_extras.handler._logic import torch_autocast
