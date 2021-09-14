50b1fbe52fe7db27ed243aa4c32e0fde45295ccf
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-09-14 11:23:03 +0900

    Fix flake8

--- pytorch_pfn_extras/handler/__init__.py
+++ pytorch_pfn_extras/handler/__init__.py
@@ -2,4 +2,4 @@ from pytorch_pfn_extras.handler._handler import BaseHandler, Handler  # NOQA
 from pytorch_pfn_extras.handler._logic import BaseLogic, Logic  # NOQA
 
 # Deprecated, only imported for backward compatibility
-from pytorch_pfn_extras.handler._logic import torch_autocast
+from pytorch_pfn_extras.handler._logic import torch_autocast  # NOQA
