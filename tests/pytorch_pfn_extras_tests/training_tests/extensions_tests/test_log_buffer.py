3c447e7ff172fc91e6b77983e77a2860277a1af5
Akifumi Imanishi <imanishi@preferred.jp>
2021-09-28 08:20:55 +0000

    Add append mode test

--- tests/pytorch_pfn_extras_tests/training_tests/extensions_tests/test_log_buffer.py
+++ tests/pytorch_pfn_extras_tests/training_tests/extensions_tests/test_log_buffer.py
@@ -1,8 +1,15 @@
-from pytorch_pfn_extras.training.extensions import log_report
+import io
+import os
+import tempfile
+import yaml
+
+import pytorch_pfn_extras as ppe
+from pytorch_pfn_extras.training import extensions
+from pytorch_pfn_extras.training.extensions import log_report as log_report_module
 
 
 def test_log_buffer():
-    buf = log_report._LogBuffer()
+    buf = log_report_module._LogBuffer()
     looker = buf.emit_new_looker()
     assert buf.size() == 0
     buf.append('mes1')
@@ -23,7 +30,7 @@ def test_log_buffer():
 
 
 def test_log_buffer_multiple_lookers():
-    buf = log_report._LogBuffer()
+    buf = log_report_module._LogBuffer()
     looker1 = buf.emit_new_looker()
     looker2 = buf.emit_new_looker()
     buf.append('mes1')
@@ -40,3 +47,55 @@ def test_log_buffer_multiple_lookers():
     assert buf.size() == 2
     looker1.clear()
     assert buf.size() == 0
+
+
+def test_buffer_size_log_report():
+    max_epochs = 10
+    iters_per_epoch = 5
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        manager = ppe.training.ExtensionsManager(
+            {}, {}, max_epochs, iters_per_epoch=iters_per_epoch, out_dir=tmpdir)
+
+        log_report = extensions.LogReport(
+            filename='out', format='yaml', append=True)
+        manager.extend(log_report, (1, 'epoch'))
+
+        for _ in range(max_epochs):
+            for _ in range(iters_per_epoch):
+                assert log_report._log_buffer.size() <= 1
+                with manager.run_iteration():
+                    pass
+
+        with open(os.path.join(tmpdir, 'out')) as f:
+            data = f.read()
+            values = yaml.load(data, Loader=yaml.SafeLoader)
+            assert len(values) == max_epochs
+
+
+def test_buffer_size_log_report_and_print_report():
+    max_epochs = 10
+    iters_per_epoch = 5
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        manager = ppe.training.ExtensionsManager(
+            {}, {}, max_epochs, iters_per_epoch=iters_per_epoch, out_dir=tmpdir)
+
+        log_report = extensions.LogReport(
+            filename='out', format='yaml', append=True)
+        manager.extend(log_report, trigger=(1, 'epoch'))
+
+        out = io.StringIO()
+        print_report = extensions.PrintReport(out=out)
+        manager.extend(print_report, trigger=(3, 'epoch'))
+
+        for _ in range(max_epochs):
+            for _ in range(iters_per_epoch):
+                assert log_report._log_buffer.size() <= 3
+                with manager.run_iteration():
+                    pass
+
+        with open(os.path.join(tmpdir, 'out')) as f:
+            data = f.read()
+            values = yaml.load(data, Loader=yaml.SafeLoader)
+            assert len(values) == max_epochs
