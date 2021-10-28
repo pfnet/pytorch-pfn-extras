df5ab394796ee25a942215d4929d9eb094a6b0ec
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-10-28 07:07:48 +0000

    fix namedtuple use in logic

--- pytorch_pfn_extras/handler/_logic.py
+++ pytorch_pfn_extras/handler/_logic.py
@@ -190,6 +190,7 @@ class Logic(BaseLogic):
 
     def _forward(self, model: torch.nn.Module, batch: Any) -> Any:
         if isinstance(batch, tuple) and hasattr(batch, '_fields'):
+            # namedtuple
             return model(batch)
         if isinstance(batch, dict):
             return model(**batch)
@@ -199,8 +200,8 @@ class Logic(BaseLogic):
 
     def _normalize_outputs(self, outputs: Any) -> Dict[str, Any]:
         if isinstance(outputs, tuple) and hasattr(outputs, '_fields'):
-            fields = outputs._fields  # type: ignore[attr-defined]
-            target = {k: getattr(outputs, k) for k in fields}
+            # namedtuple
+            target = outputs._asdict()
         elif isinstance(outputs, dict):
             target = outputs
         elif isinstance(outputs, (list, tuple)):
