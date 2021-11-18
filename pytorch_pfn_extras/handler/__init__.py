38ace88ed3e87efda898ad79e065b6ae453addb3
Emilio Castillo <ecastill@preferred.jp>
2021-11-18 07:16:33 +0000

    Expose codeblock

--- pytorch_pfn_extras/handler/__init__.py
+++ pytorch_pfn_extras/handler/__init__.py
@@ -1,3 +1,4 @@
+from pytorch_pfn_extras.handler._code_block import CodeBlock, update_parameters, forward  # NOQA
 from pytorch_pfn_extras.handler._handler import BaseHandler, Handler  # NOQA
 from pytorch_pfn_extras.handler._handler import DeferredResult  # NOQA
 from pytorch_pfn_extras.handler._logic import BaseLogic, Logic, CodeBlockLogic  # NOQA
