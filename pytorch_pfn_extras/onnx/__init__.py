2283bef492df671b5f160250b01f32dd016c382a
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-12-06 04:19:29 +0000

    add ppe.onnx.available flag

--- pytorch_pfn_extras/onnx/__init__.py
+++ pytorch_pfn_extras/onnx/__init__.py
@@ -1,10 +1,14 @@
-from pytorch_pfn_extras.onnx.export_testcase import export  # NOQA
-from pytorch_pfn_extras.onnx.export_testcase import export_testcase  # NOQA
-from pytorch_pfn_extras.onnx.export_testcase import is_large_tensor  # NOQA
-from pytorch_pfn_extras.onnx.export_testcase import LARGE_TENSOR_DATA_THRESHOLD  # NOQA
-from pytorch_pfn_extras.onnx.annotate import annotate  # NOQA
-from pytorch_pfn_extras.onnx.annotate import apply_annotation  # NOQA
-from pytorch_pfn_extras.onnx.annotate import scoped_anchor  # NOQA
-from pytorch_pfn_extras.onnx._as_output import as_output  # NOQA
-from pytorch_pfn_extras.onnx._grad import grad  # NOQA
-from pytorch_pfn_extras.onnx.load import load_model  # NOQA
+try:
+    from pytorch_pfn_extras.onnx.export_testcase import export  # NOQA
+    from pytorch_pfn_extras.onnx.export_testcase import export_testcase  # NOQA
+    from pytorch_pfn_extras.onnx.export_testcase import is_large_tensor  # NOQA
+    from pytorch_pfn_extras.onnx.export_testcase import LARGE_TENSOR_DATA_THRESHOLD  # NOQA
+    from pytorch_pfn_extras.onnx.annotate import annotate  # NOQA
+    from pytorch_pfn_extras.onnx.annotate import apply_annotation  # NOQA
+    from pytorch_pfn_extras.onnx.annotate import scoped_anchor  # NOQA
+    from pytorch_pfn_extras.onnx._as_output import as_output  # NOQA
+    from pytorch_pfn_extras.onnx._grad import grad  # NOQA
+    from pytorch_pfn_extras.onnx.load import load_model  # NOQA
+    available = True
+except ImportError:
+    available = False
