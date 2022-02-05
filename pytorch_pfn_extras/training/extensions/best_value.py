acaca05cc07c0cccf7d54dea2424a343e68be2b8
Daichi SUZUO <belltailjp@gmail.com>
2022-02-05 15:32:32 +0900

    Based on BestValueTrigger

---
+++ pytorch_pfn_extras/training/extensions/best_value.py
@@ -0,0 +1,145 @@
+from typing import Any, Callable, Dict, Optional, TYPE_CHECKING
+
+from pytorch_pfn_extras import reporting
+from pytorch_pfn_extras.training import extension
+from pytorch_pfn_extras.training import trigger as trigger_module
+from pytorch_pfn_extras.training._manager_protocol import ExtensionsManagerProtocol
+
+
+if TYPE_CHECKING:
+    from pytorch_pfn_extras.training._trigger_util import TriggerLike
+
+
+class BestValue(extension.Extension):
+
+    """Extension traces the best value of a specific key in the observation.
+
+    Args:
+        key (str): Key of value.
+        compare (callable): Compare function which takes current best value and
+            new value and returns whether new value is better than current
+            best.
+        trigger: Trigger that decides the comparison interval between current
+            best value and new value. This must be a tuple in the form of
+            ``<int>, 'epoch'`` or ``<int>, 'iteration'`` which is passed to
+            :class:`~pytorch_pfn_extras.triggers.IntervalTrigger`.
+    """
+
+    default_name = 'best_value'
+
+    def __init__(
+            self,
+            key: str,
+            compare: Callable[[float, float], bool],
+            trigger: 'TriggerLike' = (1, 'epoch'),
+    ) -> None:
+        self._key = key
+        self._best_value: Optional[float] = None
+        self._best_epoch: Optional[int] = None
+        self._best_it: Optional[int] = None
+        self._interval_trigger = trigger_module.get_trigger(trigger)
+        self._init_summary()
+        self._compare = compare
+
+    def __call__(self, manager: ExtensionsManagerProtocol) -> None:
+        observation = manager.observation
+        summary = self._summary
+        key = self._key
+        if key in observation:
+            summary.add({key: observation[key]})
+
+        if not self._interval_trigger(manager):
+            return
+
+        stats = summary.compute_mean()
+        if key not in stats:
+            raise RuntimeError('Key "{}" not found in the observation '
+                               '(current available keys are {})'
+                               .format(key, list(stats.keys())))
+
+        value = float(stats[key])  # copy to CPU
+        self._init_summary()
+
+        if self._best_value is None or self._compare(self._best_value, value):
+            self._best_value = value
+            self._best_it, self._best_epoch = manager.iteration, manager.epoch
+
+    def _init_summary(self) -> None:
+        self._summary = reporting.DictSummary()
+
+    def _check_best_value_exists(self) -> None:
+        if self._best_value is None:
+            raise RuntimeError("Best observation hasn't been obtained. "
+                               "Run the BestValue extension at least once")
+
+    @property
+    def best_value(self) -> float:
+        """Returns the current best value.
+
+        If no value has been observed yet, it raises a RuntimError.
+        """
+        self._check_best_value_exists()
+        return self._best_value  # type: ignore[return-value]
+
+    @property
+    def best_iteration(self) -> int:
+        """Returns the iteration count that the current best value is observed.
+
+        If no value has been observed yet, it raises a RuntimError.
+        """
+        self._check_best_value_exists()
+        return self._best_it  # type: ignore[return-value]
+
+    @property
+    def best_epoch(self) -> int:
+        """Returns the epoch count that the current best value is observed.
+
+        If no value has been observed yet, it raises a RuntimError.
+        """
+        self._check_best_value_exists()
+        return self._best_epoch  # type: ignore[return-value]
+
+    def state_dict(self) -> Dict[str, Any]:
+        return {
+            '_interval_trigger': self._interval_trigger.state_dict(),
+            '_summary': self._summary.state_dict(),
+            '_best_value': self._best_value,
+            '_best_it': self._best_it,
+            '_best_epoch': self._best_epoch
+        }
+
+    def load_state_dict(self, to_load: Dict[str, Any]) -> None:
+        self._interval_trigger.load_state_dict(to_load['_interval_trigger'])
+        self._summary.load_state_dict(to_load['_summary'])
+        self._best_value = to_load['_best_value']
+        self._best_it = to_load['_best_it']
+        self._best_epoch = to_load['_best_epoch']
+
+
+class MaxValue(BestValue):
+
+    """Trigger invoked when specific value becomes maximum.
+    For example you can use this trigger to take snapshot on the epoch the
+    validation accuracy is maximum.
+    Args:
+        key (str): Key of value. The trigger fires when the value associated
+            with this key becomes maximum.
+        trigger: Trigger that decides the comparison interval between current
+            best value and new value. This must be a tuple in the form of
+            ``<int>, 'epoch'`` or ``<int>, 'iteration'`` which is passed to
+            :class:`~pytorch_pfn_extras.triggers.IntervalTrigger`.
+    """
+
+    default_name = 'max_value'
+
+    def __init__(self, key: str, trigger: 'TriggerLike' = (1, 'epoch')):
+        super().__init__(
+            key, lambda max_value, new_value: new_value > max_value, trigger)
+
+
+class MinValue(BestValue):
+    default_name = 'min_value'
+
+    def __init__(self, key: str, trigger: 'TriggerLike' = (1, 'epoch')):
+        super().__init__(
+            key, lambda min_value, new_value: new_value < min_value, trigger)
