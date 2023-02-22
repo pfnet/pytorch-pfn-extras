807c7dc38f4a7070444addc080523b5a6e0f10f7
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2023-02-22 11:40:40 +0900

    Add type hint

--- pytorch_pfn_extras/onnx/_helper.py
+++ pytorch_pfn_extras/onnx/_helper.py
@@ -15,7 +15,7 @@ def _detach(x: Any) -> Any:
         return x
 
 
-def no_grad(fn: Callable[..., Any], *args, **kwargs) -> Any:
+def no_grad(fn: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
     with torch.no_grad():  # type: ignore[no-untyped-call]
         out = fn(*args, **kwargs)
     # torch.no_grad() does not export `detach` op when tracing
