25db86bd54c640d6c713e74014a0b81330f7061b
Linsho Kaku <linsho@preferred.jp>
2023-04-03 18:01:11 +0900

    Add a Logic class for clousure mode optimization.

--- pytorch_pfn_extras/handler/_logic.py
+++ pytorch_pfn_extras/handler/_logic.py
@@ -1,5 +1,6 @@
 import contextlib
-from typing import Any, Dict, Generator, Iterable, Mapping, Optional
+import dataclasses
+from typing import Any, Dict, Generator, Iterable, Mapping, Optional, Set
 import warnings
 
 from pytorch_pfn_extras.handler._code_block import forward, update_parameters
@@ -464,3 +465,197 @@ class CodeBlockLogic(BaseLogic):
         model = models[self.model_name]
         outs = forward(model)(batch)
         return outs
+
+@dataclasses.dataclass
+class ClousureModelOutput:
+    outs: Any
+    loss: torch.Tensor
+
+    def __float__(self):
+        return float(self.loss)
+    
+class ClousureLogic(BaseLogic):
+
+    def __init__(
+            self,
+            model_name: str = 'main',
+            options: Optional[Dict[str, Any]] = None,
+    ) -> None:
+        """A set of methods that defines the training logic.
+
+        Args:
+            model_name (str): Name of the model. Default is ``'main'``.
+            options (dict, optional): The configuration options.
+
+                * ``'backward_outputs'`` (list of str):
+                    A list of names of outputs that require compution of
+                    the gradient.
+                * ``'autocast'`` (bool):
+                    If ``True``, ``torch.cuda.amp.autocast`` is enabled.
+                    Default is ``False``.
+        """
+        super().__init__(options)
+        self.model_name = model_name
+
+    def consume_options(self, options: Dict[str, Any]) -> None:
+        super().consume_options(options)
+
+        self.backward_outputs = options.pop('backward_outputs', None)
+        self._grad_scaler = options.pop('grad_scaler', None)
+        self._autocast = options.pop('autocast', False)
+        self._backward_fn = options.pop('backward_function', None)
+
+        if not _amp_enabled:
+            if self._grad_scaler is not None or self._autocast:
+                raise RuntimeError('Requested AMP features but torch.cuda.amp'
+                                   ' is not enabled')
+
+        if self._grad_scaler is not None:
+            if not isinstance(self._grad_scaler, torch.cuda.amp.GradScaler):
+                raise RuntimeError('grad_scaler should be a '
+                                   'torch.cuda.amp.GradScaler object')
+            else:
+                raise RuntimeError('torch.cuda.amp.GradScaler does not support clousure step mode.')
+            
+    def _forward(self, model: torch.nn.Module, batch: Any) -> Any:
+        if isinstance(batch, tuple) and hasattr(batch, '_fields'):
+            # namedtuple
+            return model(batch)
+        if isinstance(batch, dict):
+            return model(**batch)
+        if isinstance(batch, (list, tuple)):
+            return model(*batch)
+        return model(batch)
+
+    def _get_to_backward(self, outputs: Dict[str, Any]) -> Set[torch.Tensor]:
+        to_backward = set()
+        if self.backward_outputs is None:
+            for _, v in outputs.items():
+                if isinstance(v, torch.Tensor) and v.grad_fn is not None and (
+                    (
+                        v.numel() == 1
+                        and (v.dtype.is_floating_point or v.dtype.is_complex)
+                    )
+                ):
+                    to_backward.add(v)
+        else:
+            # If backward is requested, we tried to execute it no matter the
+            # shape or type of the tensor to make the user aware
+            backward_outputs = self.backward_outputs
+            if type(backward_outputs) is str:
+                backward_outputs = (backward_outputs,)
+            for k in backward_outputs:
+                try:
+                    v = outputs[k]
+                    if isinstance(v, torch.Tensor) and v.grad_fn is not None:
+                        to_backward.add(v)
+                except KeyError:
+                    warnings.warn(
+                        'Couldn\'t find requested backward value: '
+                        f'{k} in {outputs.keys()}'
+                    )
+        return to_backward
+
+    def _backward(self, to_backward: Set[torch.Tensor]) -> None:
+        for v in to_backward:
+            if self._backward_fn is None:
+                v.backward()  # type: ignore[no-untyped-call]
+            else:
+                self._backward_fn(v)
+
+    def train_epoch_begin(
+            self,
+            models: Mapping[str, torch.nn.Module],
+            epoch: int,
+            loader: Iterable[Any],
+    ) -> None:
+        """A method called when starting a new epoch of training.
+
+        Args:
+            epoch (int): Number of epochs already finished.
+            models (dict of torch.nn.Module): The models.
+            loader (torch.utils.data.DataLoader): The data loder.
+        """
+        model = models[self.model_name]
+        model.train()
+        if hasattr(loader, 'sampler') and hasattr(
+                loader.sampler, 'set_epoch'):  # type: ignore[attr-defined]
+            # Needed for `torch.utils.data.DistributedSampler`
+            loader.sampler.set_epoch(epoch)  # type: ignore[attr-defined]
+
+    def train_step(
+            self,
+            models: Mapping[str, torch.nn.Module],
+            optimizers: Mapping[str, torch.optim.Optimizer],
+            batch_idx: int,
+            batch: Any,
+    ) -> Any:
+        """A method invokes the model forward and backward passes.
+
+        Optimizing is left to `train_step_optimizers` since maybe the user
+        would like to aggregate the gradients of several iterations.
+
+        Args:
+            models (dict of torch.nn.Module):
+                The models.
+            optimizers (dict of torch.optim.Optimizer):
+                The optimizers.
+            batch_idx (int):
+                Number of training steps already finished.
+            batch (torch.Tensor, list of torch.Tensor, dict of torch.Tensor):
+                Input tensors feeded to the model of the current step.
+        """
+        def clousure():
+            with torch_autocast(enabled=self._autocast):
+                optimizers[self.model_name].zero_grad()
+                outs = self._forward(models[self.model_name], batch)
+            to_back_outs = _normalize_outputs(outs)
+            to_backward = self._get_to_backward(to_back_outs)
+            if len(to_backward) > 1:
+                raise RuntimeError("Clousure step with multiple outputs is not supported.")
+            elif len(to_backward) == 0:
+                raise RuntimeError("No backward target found.")
+            assert len(to_backward) == 1, "There must always be one backward target."
+            self._backward(to_backward)
+            loss, = to_backward
+            return ClousureModelOutput(
+                outs=outs,
+                loss=loss,
+            )
+        
+        optimizer = optimizers[self.model_name]
+        clousure_model_output = optimizer.step(clousure)
+        if not isinstance(clousure_model_output, ClousureModelOutput):
+            raise RuntimeError(f"{type(clousure_model_output)} type object returned from optimizer.step with clousure. optimizer.step is expected to return ppe.handler.ClousureModelOutput.")
+        return clousure_model_output.outs
+
+    def train_validation_begin(
+            self,
+            models: Mapping[str, torch.nn.Module],
+    ) -> None:
+        """A method called when starting a validation.
+
+        Args:
+            models (dict of torch.nn.Module): The models.
+        """
+        model = models[self.model_name]
+        model.eval()
+
+    def eval_step(
+            self,
+            models: Mapping[str, torch.nn.Module],
+            batch_idx: int,
+            batch: Any,
+    ) -> Any:
+        """A method for an evaluation step.
+
+        Args:
+            models (dict of torch.nn.Module): The models.
+            batch_idx (int): Number of steps already finished.
+            batch (torch.Tensor, list of torch.Tensor, dict of torch.Tensor):
+                Input tensors feeded to the model of the current step.
+        """
+        model = models[self.model_name]
+        outs = self._forward(model, batch)
+        return outs
+    
