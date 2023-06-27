bf572b424c123877ca88a7183bba21c3c2245653
Emilio Castillo <ecastill@preferred.jp>
2023-06-27 08:09:56 +0000

    cleanup

--- stubs/torch/fx/__init__.pyi
+++ stubs/torch/fx/__init__.pyi
@@ -1,6 +1,6 @@
 # flake8: noqa
 from .graph import Graph as Graph
-from .graph_module import GraphModule
+from .graph_module import GraphModule as GraphModule
 from .node import Node as Node
 from .proxy import GraphAppendingTracer as GraphAppendingTracer
 from .proxy import Proxy as Proxy
