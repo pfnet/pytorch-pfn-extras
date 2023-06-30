b8c5d0f455cac16c1ba84cae2a11a2d21f67cc36
twata <twata@preferred.jp>
2023-06-30 06:08:36 +0000

    Skip tests in old torch

--- tests/pytorch_pfn_extras_tests/onnx_tests/test_export.py
+++ tests/pytorch_pfn_extras_tests/onnx_tests/test_export.py
@@ -2,6 +2,7 @@ import onnx
 import pytest
 import torch
 
+import pytorch_pfn_extras as ppe
 from pytorch_pfn_extras_tests.onnx_tests.utils import run_model_test
 
 
@@ -391,6 +392,9 @@ def test_persistent(persistent):
     assert len(model.graph.input) == (2 if persistent else 1)
 
 def test_script_device():
+    if not ppe.requires("1.12"):
+        pytest.skip("prim::device not supported before torch 1.12")
+
     @torch.jit.script
     def _select_by_mask_values(
         masks: torch.Tensor, mask_values: torch.Tensor
