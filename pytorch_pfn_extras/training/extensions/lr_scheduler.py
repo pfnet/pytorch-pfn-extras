7a2d11b9e36699ac65f67d860c9c05cca7cb1dad
Linsho Kaku <linsho@preferred.jp>
2023-08-10 21:52:30 +0900

    add comment and refactor

--- pytorch_pfn_extras/training/extensions/lr_scheduler.py
+++ pytorch_pfn_extras/training/extensions/lr_scheduler.py
@@ -48,9 +48,9 @@ class LRScheduler(extension.Extension):
         stepper (callable): Function that performs the step on
             the scheduler.
         trigger: Frequency to call this extension.
-        wait_for_first_optimizer_step (bool): Wait until optimizer.step is called
-            before invoking scheduler.step. This can address the issue where
-            optimizer.step is not called from the first iteration when using GradScaler.
+        wait_for_first_optimizer_step (bool): Wait until `optimizer.step()` is called
+            before invoking `scheduler.step()`. This can address the issue where
+            `optimizer.step()` is not called from the first iteration when using GradScaler.
     """
 
     def __init__(
@@ -66,19 +66,17 @@ class LRScheduler(extension.Extension):
         self.trigger = trigger_module.get_trigger(trigger)
         self.stepper = stepper
         self.wait_for_first_optimizer_step = wait_for_first_optimizer_step
-        self._stepped = False
         self.is_async = is_async
 
     def __call__(self, manager: ExtensionsManagerProtocol) -> None:
-        if not self._stepped:
-            if (
-                self.wait_for_first_optimizer_step
-                and self.scheduler.optimizer._step_count < 1
-            ):
-                return
-            self._stepped = True
-
-        self.stepper(manager, self.scheduler)
+        # https://github.com/pytorch/pytorch/blob/v2.0.1/torch/optim/lr_scheduler.py#L137-L138
+        if (
+            self.scheduler.optimizer._step_count < 1
+            and self.wait_for_first_optimizer_step
+        ):
+            return
+        if self.scheduler.optimizer._step_count > 0:
+            self.stepper(manager, self.scheduler)
 
     @staticmethod
     def step_by_value(key: Optional[str]) -> Any:
