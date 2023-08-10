c181c4b2f23ce811bc4ba19c2c1daccab6b17002
Linsho Kaku <linsho@preferred.jp>
2023-08-10 16:55:23 +0900

    rename templates directory

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ extensions = [
 autodoc_typehints = "description"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["templates"]
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
