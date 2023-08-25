3940385c5b8ed35d5f989f405f4cea5da557955b
Takeshi Watanabe <take-cheeze@users.noreply.github.com>
2023-08-25 14:40:59 +0900

    Update pytorch_pfn_extras/cuda/_allocator.py

--- pytorch_pfn_extras/cuda/_allocator.py
+++ pytorch_pfn_extras/cuda/_allocator.py
@@ -23,7 +23,7 @@ def stream(stream: Optional[torch.cuda.Stream]) -> Generator[None, None, None]:
     with torch.cuda.stream(stream):
         if is_available():
             cupy_stream = cupy.cuda.ExternalStream(
-                stream.cuda_stream, device_id=stream.device_index
+                stream.cuda_stream, device_id=stream.device.index
             )
             with cupy_stream:
                 yield
