ebf361d8976120a25d7d5e5f629b850eb4502fea
Linsho Kaku <linsho@preferred.jp>
2023-10-16 18:18:27 +0900

    fix type

--- pytorch_pfn_extras/training/extensions/accumulate/_accumulate_utils.py
+++ pytorch_pfn_extras/training/extensions/accumulate/_accumulate_utils.py
@@ -6,7 +6,7 @@ T = TypeVar("T")
 
 
 def all_gather_object(obj: T) -> List[Optional[T]]:
-    world_size = torch.distributed.get_world_size()
+    world_size = torch.distributed.get_world_size()  # type: ignore
     object_list: List[Optional[T]] = [None for _ in range(world_size)]
-    torch.distributed.all_gather_object(object_list=object_list, obj=obj)
+    torch.distributed.all_gather_object(object_list=object_list, obj=obj)  # type: ignore
     return object_list
