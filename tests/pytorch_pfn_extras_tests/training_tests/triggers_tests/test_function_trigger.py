5c84d4bbb17b1b5309f551f66126688554a1108c
Linsho Kaku <linsho@preferred.jp>
2023-11-17 19:35:53 +0900

    fix lint

--- tests/pytorch_pfn_extras_tests/training_tests/triggers_tests/test_function_trigger.py
+++ tests/pytorch_pfn_extras_tests/training_tests/triggers_tests/test_function_trigger.py
@@ -1,14 +1,19 @@
 from unittest.mock import MagicMock
-from pytorch_pfn_extras.training.triggers import FunctionTrigger
-from pytorch_pfn_extras.training._trigger_util import TriggerLike
-from pytorch_pfn_extras.training import trigger as trigger_module
-from pytorch_pfn_extras.training import ExtensionsManager
+
 import pytest
+from pytorch_pfn_extras.training import ExtensionsManager
+from pytorch_pfn_extras.training import trigger as trigger_module
+from pytorch_pfn_extras.training._trigger_util import TriggerLike
+from pytorch_pfn_extras.training.triggers import FunctionTrigger
+
+
 def test_function_is_called() -> None:
     fn = MagicMock()
     args = [MagicMock()]
     kwargs = {"a": MagicMock()}
-    trigger = FunctionTrigger(fn=fn, args=args, kwargs=kwargs, trigger=(1, "iteration"))
+    trigger = FunctionTrigger(
+        fn=fn, args=args, kwargs=kwargs, trigger=(1, "iteration")
+    )
     fn.assert_not_called()
     manager = ExtensionsManager({}, {}, 1, iters_per_epoch=10)
     with manager.run_iteration():
@@ -16,12 +21,13 @@ def test_function_is_called() -> None:
     trigger(manager)
     fn.assert_called_once_with(*args, **kwargs)
 
+
 def test_trigger_with_value() -> None:
-    value = {
-        "value": False
-    }
+    value = {"value": False}
     args = [value]
-    trigger = FunctionTrigger(fn=lambda x: x["value"], args=args, trigger=(1, "iteration"))
+    trigger = FunctionTrigger(
+        fn=lambda x: x["value"], args=args, trigger=(1, "iteration")
+    )
     manager = ExtensionsManager({}, {}, 1, iters_per_epoch=10)
     with manager.run_iteration():
         pass
@@ -29,16 +35,19 @@ def test_trigger_with_value() -> None:
     value["value"] = True
     assert trigger(manager)
 
+
 @pytest.mark.parametrize(
-    "trigger, iters_per_epoch", [((1, "iteration"), 10), ((1, "epoch"), 20), ((0.123, "epoch"), 17)]
+    "trigger, iters_per_epoch",
+    [((1, "iteration"), 10), ((1, "epoch"), 20), ((0.123, "epoch"), 17)],
 )
-def test_with_interval_trigger(trigger: TriggerLike, iters_per_epoch: int) -> None:
+def test_with_interval_trigger(
+    trigger: TriggerLike, iters_per_epoch: int
+) -> None:
     trigger = trigger_module.get_trigger(trigger)
     manager = ExtensionsManager({}, [], 10, iters_per_epoch=iters_per_epoch)
-    function_trigger = FunctionTrigger(fn=lambda : True, trigger=trigger)
+    function_trigger = FunctionTrigger(fn=lambda: True, trigger=trigger)
 
     while not manager.stop_trigger:
         with manager.run_iteration():
             pass
         assert trigger(manager) == function_trigger(manager)
-    
