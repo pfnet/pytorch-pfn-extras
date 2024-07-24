915f86a5ff156a03c8c497acb9cb42118c39d61e
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2024-07-24 10:36:37 +0000

    add long_description

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ here = os.path.abspath(os.path.dirname(__file__))
 # Get __version__ variable
 exec(open(os.path.join(here, "pytorch_pfn_extras", "_version.py")).read())
 
+long_description = open(os.path.join(here, "README.md")).read()
 
 setuptools.setup(
     name="pytorch-pfn-extras",
     version=__version__,  # NOQA
     description="Supplementary components to accelerate research and "
     "development in PyTorch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Preferred Networks, Inc.",
     license="MIT License",
     install_requires=["numpy", "packaging", "torch", "typing-extensions>=3.10"],
