59772a869ebc132c50ada65801eeb8fdded37db9
Akifumi Imanishi <imanishi@preferred.jp>
2024-07-24 06:57:45 +0000

    Use importlib

--- pytorch_pfn_extras/_torch_version.py
+++ pytorch_pfn_extras/_torch_version.py
@@ -1,12 +1,8 @@
+import importlib
+
 from packaging.version import Version
 
 
 def requires(version: str, package: str = "torch") -> bool:
-    if package == "torch":
-        import torch as module
-    elif package == "onnx":
-        import onnx as module
-    else:
-        raise ValueError(f"Unsupported package: {package}")
-    pkg_ver = module.__version__
+    pkg_ver = importlib.metadata.version(package)
     return Version(pkg_ver.split("+")[0].split("-")[0]) >= Version(version)
