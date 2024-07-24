c145e63dfac0edffde656bc719db234f1d4733b0
Akifumi Imanishi <imanishi@preferred.jp>
2024-07-24 05:22:39 +0000

    Avoid using deprecated pkg_resources

--- pytorch_pfn_extras/_torch_version.py
+++ pytorch_pfn_extras/_torch_version.py
@@ -1,7 +1,12 @@
-import pkg_resources
 from packaging.version import Version
 
 
 def requires(version: str, package: str = "torch") -> bool:
-    pkg_ver = pkg_resources.get_distribution(package).version
+    if package == "torch":
+        import torch as module
+    elif package == "onnx":
+        import onnx as module
+    else:
+        raise ValueError(f"Unsupported package: {package}")
+    pkg_ver = module.__version__
     return Version(pkg_ver.split("+")[0].split("-")[0]) >= Version(version)
