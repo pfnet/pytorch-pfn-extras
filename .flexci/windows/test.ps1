2c1904bafded1a261e6cbdc1884796b377f65710
Linsho Kaku <linsho@preferred.jp>
2024-09-30 18:46:04 +0900

    wip

--- .flexci/windows/test.ps1
+++ .flexci/windows/test.ps1
@@ -77,7 +77,15 @@ RunOrDie python -m pip install -e .
 
 # Unit Test
 $Env:JUPYTER_PLATFORM_DIRS = "1"
-RunOrDie python -m pytest -m "not mpi" tests
+# RunOrDie python -m pytest -m "not mpi" tests
+$test_retval = RunWithTimeout -timeout 7200 -output ./ppe_test_log.txt -- python -m pytest -m "not mpi" tests
+if ($test_retval -ne 0) {
+    echo "------------------------------------------------------------------------------------------"
+    Get-Content ppe_test_log.txt
+    echo "------------------------------------------------------------------------------------------"
+    throw "Test failed with status $test_retval"
+}
+
 
 # Examples
 .\.flexci\windows\download_mnist.ps1
