3e13b668bb1c76fe9f765a8c1ba630bf1a9da0f3
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2026-02-16 10:48:41 +0900

    Fix onnx exporter

--- pytorch_pfn_extras/onnx/_globals.py
+++ pytorch_pfn_extras/onnx/_globals.py
@@ -3,6 +3,9 @@ import torch
 from typing import Optional
 
 
-import torch.onnx._globals
-GLOBALS = torch.onnx._globals.GLOBALS
-
+_torch_major, _torch_minor = torch.__version__.split(".")[:2]
+if (int(_torch_major), int(_torch_minor)) >= (2, 9):
+    from torch.onnx._internal.torchscript_exporter import _globals
+else:
+    from torch.onnx import _globals
+GLOBALS = _globals.GLOBALS
